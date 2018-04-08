54b2cc5defa556ffbdb9e01f66be647d9cba9df9
Nicole Martindale <nicole@Nicoles-MacBook-Pro.local>
2018-04-07 23:49:35 -0700

    Initial Commit

---
+++ Shopping Center at UCSDTests/Shopping_Center_at_UCSDTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shopping_Center_at_UCSDTests.swift
+//  Shopping Center at UCSDTests
+//
+//  Created by Nicole Martindale on 4/7/18.
+//  Copyright © 2018 DarMoNic. All rights reserved.
+//
+
+import XCTest
+@testable import Shopping_Center_at_UCSD
+
+class Shopping_Center_at_UCSDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
